673cab84bf3fffcb295177744a14bcb36ec241e7
RHamudi <ramonramos.silva16@gmail.com>
2022-10-18 10:47:45 -0300

    feat: adding cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+var cors = require("cors");
 
 const app = express();
 
 // config json response
+app.use(cors());
 app.use(express.json());
 
 // Models
@@ -17,6 +19,37 @@ const User = require("./models/User");
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo", status: "200" });
 });
+
+// Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  // check if user exists
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado" });
+  }
+
+  res.status(200).json({ user });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado!" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "token invalido!" });
+  }
+}
 
 // Register User
 app.post("/auth/register", async (req, res) => {
@@ -62,6 +95,55 @@ app.post("/auth/register", async (req, res) => {
   }
 });
 
+// Login User
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // validations
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatorio!" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatoria!" });
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuario não encontrado!" });
+  }
+
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "senha inválida!" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret,
+      { expiresIn: 1000000000 }
+    );
+
+    res.status(200).json({
+      msg: "Autenticação realizada com sucesso",
+      user: user.name,
+      id: user.id,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
+});
+
 // Credencials
 const dbUSER = process.env.DB_USER;
 const dbPASS = process.env.DB_PASS;
@@ -71,7 +153,7 @@ mongoose
     `mongodb+srv://${dbUSER}:${dbPASS}@cluster0.hffqacf.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8081);
     console.log("Conectou com o banco");
   })
   .catch((err) => {
